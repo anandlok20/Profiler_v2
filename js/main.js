c2aea3843b4667a33c5e6a20bbd72c9682d5e904
Anand Lok <34028605+anandlok20@users.noreply.github.com>
2019-10-22 17:22:04 +0530

    22/10/2019

--- js/main.js
+++ js/main.js
@@ -3,14 +3,27 @@ function submitData() {
         document.getElementById("status").innerHTML = "Password and Confirm Password are different!!";
         return false;
     } else {
-        var profile = {
-            fname: document.myForm.fname.value,
-            lname: document.myForm.lname.value,
-            gender: document.myForm.gender.value,
-            email: document.myForm.email.value,
-            pass: document.myForm.pass.value
-        };
-        localStorage.setItem(document.myForm.email.value, JSON.stringify(profile));
-        // document.getElementById("status").innerHTML = "!!Thankyou for Registration!!";
+        storeDataLS();
     }
+}
+
+//function to store data
+function storeDataLS() {
+    var profile = {
+        fname: document.myForm.fname.value,
+        lname: document.myForm.lname.value,
+        gender: document.myForm.gender.value,
+        email: document.myForm.email.value,
+        pass: document.myForm.pass.value
+    };
+    localStorage.setItem(document.myForm.email.value, JSON.stringify(profile));
+    var test = JSON.parse(localStorage.getItem("profileArray"));
+    window.alert("test:" + test);
+    if (test == null) {
+        test = [];
+        test.push(document.myForm.email.value);
+    } else {
+        test.push(document.myForm.email.value);
+    }
+    localStorage.setItem("profileArray", JSON.stringify(test));
 }
