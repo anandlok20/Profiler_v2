41fd0ff2fe88f47e40bbca3e4771c815632c38c8
Anand Lok <34028605+anandlok20@users.noreply.github.com>
2019-10-24 11:17:57 +0530

    24MB

--- js/home.js
+++ js/home.js
@@ -4,10 +4,6 @@ var uemail = url.searchParams.get("email");
 var data = localStorage.getItem(uemail);
 var obj = JSON.parse(data);
 
-window.onload = function() {
-    document.getElementById("logTitle").innerHTML += " " + obj.fname + " " + obj.lname;
-}
-
 function logout() {
     localStorage.removeItem("session@" + uemail);
     obj.status = "0";
@@ -22,8 +18,36 @@ function deRegister() {
     if (cnfMsg) {
         localStorage.removeItem("session@" + uemail);
         localStorage.removeItem(uemail);
+        var recData = JSON.parse(localStorage.getItem("profileArray"));
+        window.alert(recData);
+        var retValue = recData.pop(uemail);
+        window.alert(retValue);
+        localStorage.setItem("profileArray", JSON.stringify(recData));
         window.location.replace("./index.html");
     } else {
         return false;
+    }
+}
+
+window.onload = function() {
+    interceptor();
+    var htmlData;
+    var c = 1;
+    document.getElementById("logTitle").innerHTML += " " + obj.fname + " " + obj.lname;
+    var usArray = JSON.parse(localStorage.getItem("profileArray"));
+    for (let i = 0; i < usArray.length; i++) {
+        objNew = JSON.parse(localStorage.getItem(usArray[i]));
+        if (objNew.status == "1") {
+            htmlData = '<tr><td scope="row">' + c + '</td><td>' + objNew.fname + '</td><td>' + objNew.lname + '</td></tr>';
+            document.getElementById("activeData").innerHTML += htmlData;
+            c = c + 1;
+        }
+    }
+}
+
+function interceptor() {
+    var cnfSession = localStorage.getItem("session@" + uemail);
+    if (cnfSession == null) {
+        window.location.replace("./index.html");
     }
 }
